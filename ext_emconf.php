40df21e09b3b91a9aa32ff5afe5e8e089d0c4ea4
Francois Suter <francois@typo3.org>
2014-01-26 21:53:33 +0100

    [TASK] Reproduce extension structure
    Move the template files to their proper place and restore
    the files needed for packaging as an extension.
    
    Resolves: #55346
    Releases: 0.6
    Change-Id: I86775f91fcb46276feae0b9d2fbb8f7fb802831e
    Reviewed-on: https://review.typo3.org/27062
    Reviewed-by: Francois Suter
    Tested-by: Francois Suter

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "doc_tut_templating".
+ *
+ * Auto generated 25-01-2014 18:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Templating Tutorial',
+	'description' => 'This document replaces the old templating tutorials like "Modern Template Building". The text is available at http://docs.typo3.org/typo3cms/TemplatingTutorial/.',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '0.5.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.0.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:6:{s:12:"ext_icon.gif";s:4:"7277";s:38:"Resources/Private/Template/favicon.ico";s:4:"8718";s:50:"Resources/Private/Template/index-with-markers.html";s:4:"581e";s:37:"Resources/Private/Template/index.html";s:4:"7522";s:36:"Resources/Private/Template/style.css";s:4:"210a";s:46:"Resources/Private/Template/TypoScript-code.txt";s:4:"8596";}',
+);
+
+?>
