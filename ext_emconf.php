a647d09fd6922b19ac304b5c85a90fa239649c38
Christopher <chrissitopher@gmx.de>
2015-08-26 22:00:15 +0200

    [TASK] Maintenance update

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,42 +10,28 @@
 
 $EM_CONF[$_EXTKEY] = array (
   'title' => 'Templating Tutorial',
-  'description' => 'This document replaces the old templating tutorials like "Modern Template Building". The text is available at http://docs.typo3.org/typo3cms/TemplatingTutorial/.',
+  'description' => 'This document replaces the old templating tutorials like "Modern Template Building". The text is available at https://docs.typo3.org/typo3cms/TemplatingTutorial/.',
   'category' => 'doc',
-  'shy' => 0,
   'version' => '0.7.0',
-  'dependencies' => '',
-  'conflicts' => '',
-  'priority' => '',
-  'loadOrder' => '',
   'module' => '',
   'state' => 'beta',
   'uploadfolder' => 0,
   'createDirs' => '',
-  'modify_tables' => '',
   'clearcacheonload' => 0,
-  'lockType' => '',
   'author' => 'Official Documentation',
   'author_email' => 'documentation@typo3.org',
   'author_company' => '',
-  'CGLcompliance' => '',
-  'CGLcompliance_note' => '',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
       'typo3' => '4.5.0-6.2.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:6:{s:12:"ext_icon.gif";s:4:"7277";s:38:"Resources/Private/Template/favicon.ico";s:4:"8718";s:50:"Resources/Private/Template/index-with-markers.html";s:4:"581e";s:37:"Resources/Private/Template/index.html";s:4:"7522";s:36:"Resources/Private/Template/style.css";s:4:"210a";s:46:"Resources/Private/Template/TypoScript-code.txt";s:4:"8596";}',
-  'user' => 'docteam',
-  'comment' => 'Raised compatibility to TYPO3 CMS 6.2',
 );
-
-?>
